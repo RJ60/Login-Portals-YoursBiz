11ec0ae6859e2a19b97a239df1aa38bf7851298f
RJ60 <59600940+RJ60@users.noreply.github.com>
2021-07-25 15:02:52 +0530

    Login Portals

--- Login Portals/Influencers-Page/Login Portals_Dashboard/Influencers-Page-Dashboard.js
+++ Login Portals/Influencers-Page/Login Portals_Dashboard/Influencers-Page-Dashboard.js
@@ -1,38 +1,4 @@
-// let myChart = document.getElementById('Total_Impressions-chart').getContext('2d');
-        
-// Global options
-// Chart.defaults.global.width = 20;
-
-    // let chart = new Chart(myChart, {
-    //     type: 'bar',  //type of chart 
-    //     data:{
-    //         labels:['','','','','','','','','','',' ' ],
-    //         datasets: [{
-    //             data: [46, 14, 106, 106, 280, 411, 133, 221, 683, 247, 560, 340],
-    //             label: "",
-    //             borderColor: "#458af7",
-    //             backgroundColor: '#233A6C',
-    //             fill: false
-    //         },
-    //     ]
-
-    //     },
-    //     options:{
-    //         scales:{
-    //             xAxes: [{
-    //                 barThickness: 30,
-                    
-    //               }],
-    //         },
-    //         title:{
-    //             display:true,
-    //             text:'View the monthly impressions you get on Ad-Dekho website',
-    //             fontSize:15,
-                
-    //         }
-            
-    //     }
-    // });
+// For Drawing Graphs
 
     google.charts.load('current', { 'packages': ['bar'] });
 google.charts.setOnLoadCallback(drawChart);
@@ -80,6 +46,10 @@ function drawChart() {
         }
     }
 }
+
+// Graph ends
+
+// Code for selecting duration according to duration selected by the user in dropdown button
 
 document.getElementById("period").innerHTML = document.getElementById("time-prd").innerHTML + ':';
 document.getElementById('total').innerHTML = 'Total' + ':';
